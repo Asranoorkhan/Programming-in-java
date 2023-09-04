aadbf1f5dd36539d63c7b2e0edf6e2a4d5fa8f99
Asranoorkhan <142888731+Asranoorkhan@users.noreply.github.com>
2023-09-04 23:16:59 +0530

    Create Integer.java

---
+++ Integer.java
@@ -0,0 +1,12 @@
+// to print a given integer by User
+import java.util.Scanner;
+public class Integer
+{
+	public static void main(String[] args) {
+	    //int a;
+	    Scanner sc=new Scanner (System.in);
+		System.out.println("enter the integer ");
+		int a=sc.nextInt();
+		System.out.println(a);
+	}
+}
